04e99b8bc054f389f19bb21eab1d485a54ec155a
Ulahh <ulah57@gmail.com>
2023-06-27 05:11:28 -0700

    create place holder data that grabs the current signed in users information and pre populates the contact form;

--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import {trpc} from "../utils/trpc";
+import { useSession } from "next-auth/react";
 
 interface Props {
   // Optional props here
-  email: string
-
+  email: string;
 }
 
 const ContactPage: React.FC<Props> = ({ email }) => {
-  const [name, setName] = React.useState('');
-  const [emailAddress, setEmailAddress] = React.useState('');
-  const [message, setMessage] = React.useState('');
-  const [status, setStatus] = React.useState<'success' | 'error' | 'idle'>('idle');
+  const { data: sessionData } = useSession();
+  const checker = trpc.user.getUser.useQuery({email: `${sessionData?.user?.email}`});
+  const [message, setMessage] = React.useState("");
+  const [status, setStatus] = React.useState<"success" | "error" | "idle">("idle");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    
+  
     e.preventDefault();
-    setStatus('idle');
+    setStatus("idle");
     // try {
     //   await axios.post('/send-email', {
     //     name,
@@ -29,9 +32,9 @@ const ContactPage: React.FC<Props> = ({ email }) => {
   };
 
   return (
-    <div className="bg-gray-900 text-white min-h-screen">
-      <div className="container text-black mx-auto px-5 py-12">
-        <h1 className="text-3xl text-white font-bold mb-6">Contact Us</h1>
+    <div className="bg-white text-black min-h-screen">
+      <div className="container mx-auto px-5 py-12">
+        <h1 className="text-3xl font-bold mb-6">Contact Us</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 font-medium mb-2" htmlFor="name">
@@ -40,9 +43,10 @@ const ContactPage: React.FC<Props> = ({ email }) => {
             <input
               className="border border-gray-500 p-2 rounded-lg w-full"
               type="text"
+              readOnly={true}
               id="name"
-              value={name}
-              onChange={e => setName(e.target.value)}
+              value={`${checker.data?.firstName} ${checker.data?.lastName}`}
+              
             />
           </div>
           <div className="mb-4">
@@ -52,9 +56,10 @@ const ContactPage: React.FC<Props> = ({ email }) => {
             <input
               className="border border-gray-500 p-2 rounded-lg w-full"
               type="email"
+              readOnly={true}
               id="email"
-              value={emailAddress}
-              onChange={e => setEmailAddress(e.target.value)}
+              value={`${checker.data?.email}`}
+              
             />
           </div>
           <div className="mb-4">
@@ -65,32 +70,31 @@ const ContactPage: React.FC<Props> = ({ email }) => {
               className="border border-gray-500 p-2 rounded-lg w-full h-32"
               id="message"
               value={message}
-              onChange={e => setMessage(e.target.value)}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <div className="text-center">
             <button
-              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+              className="btn bg-orange-400"
               type="submit"
             >
               Submit
             </button>
           </div>
         </form>
-        {status === 'success' && (
-<div className="bg-green-500 text-white p-4 rounded-lg">
-  Message sent successfully!
-</div>
-)}
-{status === 'error' && (
-<div className="bg-red-500 text-white p-4 rounded-lg">
-  There was an error sending your message. Please try again later.
-</div>
-)}
+        {status === "success" && (
+          <div className="bg-green-500 text-white p-4 rounded-lg">
+            Message sent successfully!
+          </div>
+        )}
+        {status === "error" && (
+          <div className="bg-red-500 text-white p-4 rounded-lg">
+            There was an error sending your message. Please try again later.
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default ContactPage;
-
